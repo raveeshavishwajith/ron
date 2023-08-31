5723fa2412fdda4bd0a0681a2743ac77b67e9ccf
Vieronicka <121214132+vieronicka@users.noreply.github.com>
2023-08-31 19:10:45 +0530

    eventmanger_dashboard_v3

---
+++ eventmanager_dashboard/addEventForm.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style(addNew).css">
+    <title>Document</title>
+</head>
+<body>
+    <center>
+    <div class= form>
+    <form>
+        <h2> Add New Event</h2>
+        <label for="orgName">Name of the Organisation: </label>
+        <input type="text" name="orgName" value="" id="orgName" required>
+
+        <label for="eventTitle">Title of the Event: </label>
+        <input type="text" name="eventTitle" value="" id="eventTitle" required>
+
+        <label for="eventVenue">Venue of the Event: </label>
+        <input type="text" name="eventVenue" value="" id="eventVenue" required>
+
+        <label for="voluntCount">No.of Volunteers required: </label>
+        <input type="text" name="voluntCount" value="" id="voluntCount" required>
+
+        <label for="moreDet">More details: </label>
+        <input type="text" name="moreDet" value="" id="moreDet" required>
+
+        <label for="contact">Contact details: </label>
+        <input type="text" name="contact" value="" id="contact" required>
+
+        <label for="flyer">Upload flyer: </label>
+        <input type="file" name="flyer" value="" id="flyer" required>
+
+
+        <input type="submit" name="submit" value="Submit" >
+
+
+
+
+    </form>
+    </div>
+    </center>
+</body>
+</html>
