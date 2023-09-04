8afde481b3bac08c88bca86340384e0b216c6ade
nipuni-fernando123 <nipunifernando8899@gmail.com>
2023-09-04 16:26:07 +0530

    edit

--- homePg(new)/main.js
+++ homePg(new)/main.js
@@ -2,19 +2,35 @@ const prev=document.querySelector('.prev')
 const next=document.querySelector('.next')
 const slider=document.querySelector('.slider')
 
-prev.addEventListener('click',() => {
-    slider.scrollLeft-=300
+// prev.addEventListener('click',() => {
+
+//     slider.scrollLeft-=300
+// })
+
+// prev.addEventListener('click',() => {
+//     slider.scrollRight+=300
+// })
+
+// next.addEventListener('click',() => {
+//     slider.scrollRight-=300
+// })
+
+// next.addEventListener('click',() => {
+//     slider.scrollLeft+=300
+// })
+
+const itemWidth = 300; // Adjust this to your item width in pixels
+
+prev.addEventListener('click', () => {
+    slider.scrollLeft -= itemWidth;
+    if (slider.scrollLeft < 0) {
+        slider.scrollLeft = slider.scrollWidth - slider.clientWidth;
+    }
 })
 
-prev.addEventListener('click',() => {
-    slider.scrollRight+=300
+next.addEventListener('click', () => {
+    slider.scrollLeft += itemWidth;
+    if (slider.scrollLeft >= slider.scrollWidth - slider.clientWidth) {
+        slider.scrollLeft = 0;
+    }
 })
-
-next.addEventListener('click',() => {
-    slider.scrollRight-=300
-})
-
-next.addEventListener('click',() => {
-    slider.scrollLeft+=300
-})
-
